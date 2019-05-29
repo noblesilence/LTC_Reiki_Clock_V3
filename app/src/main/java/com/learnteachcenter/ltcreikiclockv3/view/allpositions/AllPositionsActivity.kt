42dd63a6d75118a7d0c5256cc098bbc743e668ca
Aye Aye Mon <noblesilence@gmail.com>
2019-05-29 23:12:56 +0700

    Display Positions

--- app/src/main/java/com/learnteachcenter/ltcreikiclockv3/view/allpositions/AllPositionsActivity.kt
+++ app/src/main/java/com/learnteachcenter/ltcreikiclockv3/view/allpositions/AllPositionsActivity.kt
@@ -1,10 +1,18 @@
 package com.learnteachcenter.ltcreikiclockv3.view.allpositions
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import com.learnteachcenter.ltcreikiclockv3.R
+import com.learnteachcenter.ltcreikiclockv3.model.Position
 import com.learnteachcenter.ltcreikiclockv3.util.IntentExtraNames
+import com.learnteachcenter.ltcreikiclockv3.util.NetworkUtil
+import com.learnteachcenter.ltcreikiclockv3.viewmodel.AllPositionsViewModel
+import com.learnteachcenter.ltcreikiclockv3.viewmodel.AllPositionsViewModelFactory
+import kotlinx.android.synthetic.main.activity_all_positions.*
 
 class AllPositionsActivity : AppCompatActivity() {
 
@@ -12,15 +20,46 @@ class AllPositionsActivity : AppCompatActivity() {
 
     private lateinit var reikiId: String
 
+    private lateinit var viewModel: AllPositionsViewModel
+
+    private val adapter = PositionsAdapter(mutableListOf())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_positions)
+        setSupportActionBar(toolbar)
 
         val i = intent
 
         if(i.hasExtra(IntentExtraNames.EXTRA_REIKI_ID)) {
             reikiId = i.getStringExtra(IntentExtraNames.EXTRA_REIKI_ID)
             Log.d(TAG, "reikiId is ${reikiId}")
+
+            viewModel = ViewModelProviders.of(this, AllPositionsViewModelFactory(reikiId))
+                .get(AllPositionsViewModel::class.java)
+
+            initRecyclerView()
+            subscribeObservers()
+
+            // Show/hide Add button based on internet connectivity
+            if(NetworkUtil.isConnected(this)) {
+                fab_add.setOnClickListener {
+                }
+            } else {
+                fab_add.hide()
+            }
         }
     }
+
+    private fun initRecyclerView() {
+        positionsRecyclerView.layoutManager = LinearLayoutManager(this)
+        positionsRecyclerView.adapter = adapter
+    }
+
+    private fun subscribeObservers() {
+        viewModel.positions.observe(this, Observer<List<Position>> {positions ->
+            adapter.setPositions(positions)
+        })
+    }
+
 }
--- app/src/main/java/com/learnteachcenter/ltcreikiclockv3/viewmodel/AllPositionsViewModel.kt
+++ app/src/main/java/com/learnteachcenter/ltcreikiclockv3/viewmodel/AllPositionsViewModel.kt
@@ -1,16 +1,12 @@
 package com.learnteachcenter.ltcreikiclockv3.viewmodel
 
+import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.ViewModel
+import com.learnteachcenter.ltcreikiclockv3.model.Position
+import com.learnteachcenter.ltcreikiclockv3.repository.ReikiRepository
+import com.learnteachcenter.ltcreikiclockv3.util.Injection
 
-class AllPositionsViewModel (
-//    private val repository: ReikiRepository = Injection.provideRepository(),
-    private val reikiId: String) : ViewModel() {
-
-//    val positionsObservable: LiveData<List<Position>> = repository.getPositions(reikiId)
-//
-//    fun getPositions(reikiId: String) = repository.getPositions(reikiId)
-//
-//    fun getReikisObservable(): LiveData<List<Position>> {
-//        return positionsObservable
-//    }
+class AllPositionsViewModel (reikiId: String) : ViewModel() {
+    private val repository: ReikiRepository = Injection.provideReikiRepository()
+    var positions: LiveData<List<Position>> = repository.getPositions(reikiId)
 }
